628102f04744e3587786dccebdbae8fa3a3d6059
henry-49 <henryswebdevelopment@gmail.com>
2021-07-10 01:35:09 +0200

    Setup Forget and Reset Password for Frontend Template

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,36 +1,71 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+@extends('frontend.main_master')
 
-        <x-jet-validation-errors class="mb-4" />
+@section('content')
+<div class="breadcrumb">
+	<div class="container">
+		<div class="breadcrumb-inner">
+			<ul class="list-inline list-unstyled">
+				<li><a href="home.html">Home</a></li>
+				<li class='active'>Reset Password</li>
+			</ul>
+		</div><!-- /.breadcrumb-inner -->
+	</div><!-- /.container -->
+</div><!-- /.breadcrumb -->
 
-        <form method="POST" action="{{ route('password.update') }}">
+<div class="body-content">
+	<div class="container">
+		<div class="sign-in-page">
+			<div class="row">
+				<!-- Sign-in -->			
+    <div class="col-md-6 col-sm-6 sign-in">
+        <h4 class="">Reset Password</h4>
+       
+        <form method="POST" action="{{ route('password.update') }}"class="register-form outer-top-xs" role="form">
             @csrf
 
             <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-            <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
+            <div class="form-group">
+                <label class="info-title" for="exampleInputEmail1">Email Address <span>*</span></label>
+                <input type="email" name="email" :value="old('email', $request->email)" required autofocus class="form-control unicase-form-control text-input" id="email" >
+                @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
             </div>
+            <div class="form-group">
+                <label class="info-title" for="exampleInputEmail1">Password <span>*</span></label>
+                <input type="password" class="form-control unicase-form-control" id="password"  name="password">
+                @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label class="info-title" for="exampleInputEmail1">Confirm Password <span>*</span></label>
+                <input type="password" class="form-control unicase-form-control" id="password_confirmation"  name="password_confirmation">
+                @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
+            
+            <button type="submit" class="btn-upper btn btn-primary checkout-page-button">Reset Password</button>
+        </form>					
+    </div>
+    <!-- Sign-in -->
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
+    <!-- create a new account -->
+   
+<!-- create a new account -->			</div><!-- /.row -->
+		</div><!-- /.sigin-in-->
+		<!-- ============================================== BRANDS CAROUSEL ============================================== -->
 
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-jet-button>
-                    {{ __('Reset Password') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+    @include('frontend.body.brands')
+<!-- ============================================== BRANDS CAROUSEL : END ============================================== -->	
+</div><!-- /.container -->
+</div><!-- /.body-content -->
+@endsection
